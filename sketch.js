252e3c1df729775b439acc34e31a764546174ddb
adipathania06 <61343011+adipathania06@users.noreply.github.com>
2020-03-26 05:12:20 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var cannonball,tanker;
+
+
+
+function setup() {
+    // Setup the canvas, the ground the, tanker, the shooting ball and the bubble balls.
+   var canvas = createCanvas(900,400);
+     
+
+   var tanker = new Tanker 
+}
+
+function draw() {
+  tanker.display();
+}
+
+
+function keyReleased() {
+    // Call the shoot method for the cannon.
+}
